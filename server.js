3acad0f17b0e9810f492a692d5711b44a54ad35b
zjeffreys <zjeffreys@seattlu.edu>
2022-01-11 17:26:57 -0800

    removed ssl https method

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
-function requireHTTPS(req, res, next) {
-    // The 'x-forwarded-proto' check is for Heroku
-    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-        return res.redirect('https://' + req.get('host') + req.url);
-    }
-    next();
-}
+// function requireHTTPS(req, res, next) {
+//     // The 'x-forwarded-proto' check is for Heroku
+//     if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+//         return res.redirect('https://' + req.get('host') + req.url);
+//     }
+//     next();
+// }
 
 const express = require('express');
 const path = require('path'); 
 const app = express();
 
-// Hack: https isn't working on local host, so use http on localhost:8080 and https on Heroku Production
-if(__dirname =="/Users/zachary/Documents/GitHub/kraken" ){
-    console.log("dev enviroment running")
-} else {
-    app.use(requireHTTPS);
-}
+// // Hack: https isn't working on local host, so use http on localhost:8080 and https on Heroku Production
+// if(__dirname =="/Users/zachary/Documents/GitHub/kraken" ){
+//     console.log("dev enviroment running")
+// } else {
+//     app.use(requireHTTPS);
+// }
 app.use(express.static(path.join(__dirname, '/dist/kraken')));
 
 app.get('/*', (req, res) =>
