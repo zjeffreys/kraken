5c0abe4e280d800297fbb597cbc79efac27754d9
zjeffreys <zjeffreys@seattlu.edu>
2022-01-14 02:34:32 -0800

    fixed hero, investor, team, added images

--- src/app/investor/investor.component.ts
+++ src/app/investor/investor.component.ts
@@ -6,8 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./investor.component.css']
 })
 export class InvestorComponent implements OnInit {
+  investor_data:any={ 
+    'package': "Legacy Kraken Package", 
+    'contains': [
+      "1 X First 100 Crypto Kraken", 
+      "0.5% Equity in crypto_kraken.co",
+      "1 X Gold Weapon", 
+      "$500 Investor Buy In",
+    ]
+  }
+  opportunities: any[] = [];
+  constructor() { 
+    for (let index = 0; index < 6; index++) {
+      this.opportunities.push(this.investor_data); 
 
-  constructor() { }
+    }
+  }
 
   ngOnInit(): void {
   }
--- src/app/team/team.component.ts
+++ src/app/team/team.component.ts
@@ -6,8 +6,109 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./team.component.css']
 })
 export class TeamComponent implements OnInit {
-
-  constructor() { }
+  INVESTOR:any[]=[
+    {
+      'name': 'TBD', 
+      'price': '500', 
+    }, 
+    {
+      'name': 'TBD', 
+      'price': '500', 
+    }, 
+    {
+      'name': 'TBD', 
+      'price': '500', 
+    }, 
+    {
+      'name': 'TBD', 
+      'price': '500', 
+    }, 
+    {
+      'name': 'TBD', 
+      'price': '500', 
+    }, 
+    {
+      'name': 'TBD', 
+      'price': '500', 
+    }
+  ]
+  TEAM: any[] = [
+    {
+      'name': 'Zachary Jeffreys', 
+      'profile_img': '/assets/img/meNBuddah.jpg',
+      'title': 'Developer & Data Scientist', 
+      'kraken_imgs': ['/assets/img/kraken_lava.png'], 
+      'contact': 'zjeffreys@seattleu.edu', 
+      'requirements': '',
+    }, 
+    {
+      'name': 'Nichalos Jeffreys', 
+      'profile_img': 'https://media-exp1.licdn.com/dms/image/C5603AQGo8Kr9e1Btjw/profile-displayphoto-shrink_200_200/0/1568095097522?e=1647475200&v=beta&t=KfSKesInJbx93U6ke6Xkafle3-_oX7VaR1OHmWelGJQ',
+      'title': 'TBD', 
+      'kraken_imgs': ['/assets/img/kraken_posiden.png'], 
+      'contact': '253-905-2653', 
+      'requirements': '', 
+    },  
+    {
+      'name': 'TBD', 
+      'profile_img': '/assets/img/Level.png',
+      'title': 'Discord Manager', 
+      'kraken_imgs': ['/assets/img/kraken_posiden.png'], 
+      'contact': '', 
+      'requirements' : [
+        "Grow discord to 4,000 members 2 months",
+        "Grow Instagram to 2,000 members", 
+        "Communicate daily to engage community", 
+        "$500 Buy In"
+      
+      ], 
+      'reward': '1 X Supreme God Package', 
+    },  
+    {
+      'name': 'TBD', 
+      'profile_img': '/assets/img/Level.png',
+      'title': 'Blockchain/Full-Stack Developer', 
+      'kraken_imgs': ['/assets/img/kraken_posiden.png'], 
+      'contact': '', 
+      'requirements' : [
+        "Help to build & test application",
+        "Deploy smart contracts", 
+        "$500 Buy In"
+      
+      ], 
+      'reward': '1 X Supreme God Package', 
+    },   
+     {
+      'name': 'TBD', 
+      'profile_img': '/assets/img/Level.png',
+      'title': 'Content Creator', 
+      'kraken_imgs': ['/assets/img/kraken_posiden.png'], 
+      'contact': '', 
+      'requirements' : [
+        "Create new content daily. Video, pictures, etc.", 
+        "Work with Social Media Manager ", 
+        "$500 Buy In"
+      ], 
+      'reward': '1 X Supreme God Package', 
+    },  
+    {
+      'name': 'TBD', 
+      'profile_img': '/assets/img/Level.png',
+      'title': 'Funding Manager', 
+      'kraken_imgs': ['/assets/img/kraken_posiden.png'], 
+      'contact': '', 
+      'requirements' : [
+        "Sit and meet with potential investors ",
+        "Obtain 10,000 funding", 
+        "$500 Buy In"
+      ], 
+      'reward': '1 X Supreme God Package', 
+    },
+    
+  ]
+  constructor() {
+   
+   }
 
   ngOnInit(): void {
   }
